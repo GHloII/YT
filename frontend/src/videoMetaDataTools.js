6f03ba7676b2d93fff24666c5e68ab0f6cb3af85
Bogdan Khamidullin <bogdankhamidullin@gmail.com>
2025-08-20 21:20:27 +0500

    remove modules

--- frontend/src/videoMetaDataTools.js
+++
@@ -1,4 +0,0 @@
-async function getVideoDataFromYoutube(url) {
-    const oembedUrl = `https://www.youtube.com/oembed?url=${encodeURIComponent(url)}&format=json`;
-    return (await fetch(oembedUrl)).json()
-}
