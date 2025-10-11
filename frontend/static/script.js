cd55929ee03a622fc2cafc09bcd1d6793f1b9887
Napordisch <bogdankhamidullin@gmail.com>
2025-10-12 03:49:22 +0500

    downloading video but ffmpeg errors

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -1,5 +1,16 @@
+function optionFromValueAndLabel(value,label) {
+    return `<option value=${value}>${label}</option>`
+}
 
-const videoInfoElement = (imageLink, videoLink, videoTitle, videoAuthor, qualityOptions) => `
+function qualityNameIdPairs(qualityIdByName) {
+    return Object.entries(qualityIdByName).map(pair => ({
+        name: pair[0],
+        id: pair[1]
+    }))
+}
+
+
+const videoInfoElement = (imageLink, videoLink, videoTitle, qualityOptions, videoAuthor) => `
     <div class="video-info">
         <div class="video-preview">
             <img src="${imageLink}" class="video-preview-image" />
@@ -18,10 +29,9 @@ const videoInfoElement = (imageLink, videoLink, videoTitle, videoAuthor, quality
             </div>
             <div class="download-controls">
                 <select class="quality-select download-control">
-                    <option>1080p</option>
-                    <option>360p</option>
+                ${qualityOptions.map(opt => optionFromValueAndLabel(opt.id, opt.name))}
                 </select>
-                <button class="download-control download-button">
+                <button class="download-control download-button" onclick="downloadVideo()">
                     Скачать
                 </button>
             </div>
@@ -30,6 +40,16 @@ const videoInfoElement = (imageLink, videoLink, videoTitle, videoAuthor, quality
     `
 
 const mainVideoLinkInput = document.getElementById('main-video-link-input')
+const videoInfoContainer = document.getElementById('video-info-container')
+const qualitySelect = () => document.getElementsByClassName('quality-select')[0]
+
+async function getVideoInfo(videoURL) {
+    const params = new URLSearchParams({
+        url: videoURL
+    })
+    return (await fetch(`/info?${params}`)).json()
+}
+
 
 function videoIdByYoutubeUrl(url) {
     const parsed = new URL(url)
@@ -47,10 +67,37 @@ function videoIdByYoutubeUrl(url) {
 }
 
 function videoInfo(youtubeVideoId) {
-
-
 }
 
 function currentLinkInInput() {
     return mainVideoLinkInput.value
 }
+
+async function downloadVideo() {
+    const taskId = (await (await fetch('/getDownloadID')).json()).taskId
+    const params = new URLSearchParams({
+        url: mainVideoLinkInput.value,
+        videoId: qualitySelect().value,
+        taskId:taskId,
+        audioId:'bestaudio'
+    })
+    const a = document.createElement('a')
+    a.href = `/download?${params}`
+    console.log(a.href)
+    a.download = ``
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+}
+
+
+mainVideoLinkInput.addEventListener('input', () => {
+    const videoLink = mainVideoLinkInput.value
+    getVideoInfo(videoLink).then((res) => {
+        const {thumbnail, resolutions, title, idByQualityName} = res
+        if (thumbnail == undefined || resolutions == undefined || title == undefined || idByQualityName == undefined) {
+            throw new Error('no for this url')
+        }
+        videoInfoContainer.innerHTML = videoInfoElement(thumbnail, videoLink, title, qualityNameIdPairs(idByQualityName))
+    })
+})
